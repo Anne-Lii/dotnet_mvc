d6deefeddd6c188d8b72a74e30dd0b7a3dbb10eb
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-28 17:02:00 +0100

    skapat filstrukturen

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); //Aktiverar MVC
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();// För att kunna använda statiska filer
+app.UseRouting();//För routing
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
